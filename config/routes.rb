65451398ace47d36713846840b7e63be80405621
gumtow <gumtow@gmail.com>
2021-07-15 22:22:50 -0500

    daily commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   root to: 'home#index'
 
-  scope :api, defaults: { format: :json } do
-    devise_for :users, controllers: { sessions: :sessions },
-                      path_names: { sign_in: :login }
+  # scope :api, defaults: { format: :json } do
+  #   devise_for :users, controllers: { sessions: :sessions },
+  #                     path_names: { sign_in: :login }
 
-    resource :user, only: [:show, :update]
-  end
+  #   resource :user, only: [:show, :update]
+  # end
 
-  post ':controller(/:action(/:id(.:format)))'
-  get ':controller(/:action(/:id(.:format)))'
+  # post ':controller(/:action(/:id(.:format)))'
+  # get ':controller(/:action(/:id(.:format)))'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/rails/info/properties' , to: "rails/info#properties"
-  get '/rails/info/routes'     , to: "rails/info#routes"
-  get '/rails/info'            , to: "rails/info#index"
-  get '/'                      , to: "rails/welcome#index"
+  # get '/rails/info/properties' , to: "rails/info#properties"
+  # get '/rails/info/routes'     , to: "rails/info#routes"
+  # get '/rails/info'            , to: "rails/info#index"
+  # get '/'                      , to: "rails/welcome#index"
 end
